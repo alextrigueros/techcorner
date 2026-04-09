d318b8e992812c0829e2efa1f5033937207172a9
alextrigueros <alextrigueros.alum@insestatut.cat>
2026-04-09 02:34:25 +0200

    estil carret

--- views/carret_view.php
+++ views/carret_view.php
@@ -9,65 +9,81 @@
 
 <body>
     <?php include "views/header_view.php"; ?>
-    <h1>El teu carret</h1>
 
-    <div class="contenidor-carret">
-        <?php
-        //Si no hi ha productes mostrem el missatge de carret buit
-        if (count($productes_carret) == 0) {
-            echo "<p>El teu carret està buit. <a href='index.php?accio=botiga'>Vols anar a la botiga?</a></p>";
-        } else {
-            //Si hi ha, pintem la taula i el resum
-            echo "<table>
-                <thead>
-                    <tr>
-                        <th>Producte</th>
-                        <th>Preu Unitari</th>
-                        <th>Quantitat</th>
-                        <th>Subtotal</th>
-                        <th>Accions</th>
-                    </tr>
-                </thead>
-                <tbody>";
+    <div class="contenidor seccio-carret">
+        <h1 class="titol-carret">El teu carret</h1>
 
-            foreach ($productes_carret as $p) {
-                $id_detall = $p['detall_carret_id'];
-                $nom = $p['nom'];
-                $preu = $p['preu'];
-                $quantitat = $p['quantitat'];
-                $imatge = $p['imatge_url'];
-                //Formatem el subtotal a 2 decimals
-                $subtotal = number_format($preu * $quantitat, 2);
+        <div class="contenidor-carret">
+            <?php
+            // Si no hi ha productes al carret, mostra un missatge indicant que està buit i un botó gran
+            if (count($productes_carret) == 0): ?>
+                <div class="carret-buit">
+                    <p>El teu carret està buit.</p>
+                    <a href="index.php?accio=botiga" class="boto-primari-gran">Anar a la botiga</a>
+                </div>
+            <?php else: ?>
+                <div class="accions-globals">
+                    <a href="index.php?accio=botiga" class="boto-secundari-carret">← Seguir comprant</a>
+                </div>
+                
+                <div class="layout-carret">
 
-                echo "<tr>
-                    <td>
-                        <img src='content/products/$imatge' width='50' style='vertical-align:middle; margin-right:10px;'>
-                        $nom
-                    </td>
-                    <td>$preu €</td>
-                    <td>$quantitat</td>
-                    <td>$subtotal €</td>
-                    <td>
-                        <a href='index.php?accio=eliminar_item&id=$id_detall' style='color:red;'>Eliminar</a>
-                    </td>
-                </tr>";
-            }
+                    <div class="productes-carret">
+                        <?php foreach ($productes_carret as $p):
+                            $id_detall = $p['detall_carret_id'];
+                            $subtotal = number_format($p['preu'] * $p['quantitat'], 2);
+                        ?>
+                            <div class="targeta-carret-horitzontal">
+                                <img src="content/products/<?php echo $p['imatge_url']; ?>" alt="<?php echo $p['nom']; ?>">
 
-            echo "</tbody>
-            </table>";
+                                <div class="info-producte-carret">
+                                    <h3><?php echo $p['nom']; ?></h3>
+                                    <p class="preu-unitari"><?php echo $p['preu']; ?> € / unitat</p>
+                                </div>
 
-            $total_f = number_format($total_carret, 2);
+                                <div class="quantitat-carret">
+                                    <span>Quantitat: <strong><?php echo $p['quantitat']; ?></strong></span>
+                                </div>
 
-            echo "<div class='resum-carret'>
-                <h3>Total: $total_f €</h3>
-                <a href='index.php?accio=finalitzar_comanda' class='boto-comprar'>Finalitzar Compra</a>
-            </div>";
-        }
-        ?>
+                                <div class="subtotal-producte">
+                                    <span><?php echo $subtotal; ?> €</span>
+                                </div>
 
-        <br>
-        <a href="index.php?accio=botiga">Seguir comprant</a>
+                                <div class="accions-carret">
+                                    <a href="index.php?accio=eliminar_item&id=<?php echo $id_detall; ?>" class="enllac-eliminar">Eliminar</a>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+
+
+                    </div>
+
+                    <aside class="resum-compra">
+                        <h2>Resum de la comanda</h2>
+                        <div class="linia-resum">
+                            <span>Productes (<?php echo count($productes_carret); ?>)</span>
+                            <span><?php echo number_format($total_carret, 2); ?> €</span>
+                        </div>
+                        <div class="linia-resum">
+                            <span>Enviament</span>
+                            <span class="gratis">Gratis</span>
+                        </div>
+                        <hr>
+                        <div class="total-comanda">
+                            <span>Total (IVA inclòs)</span>
+                            <span class="import-total"><?php echo number_format($total_carret, 2); ?> €</span>
+                        </div>
+
+                        <a href="index.php?accio=finalitzar_comanda" class="boto-finalitzar">
+                            Finalitzar Compra
+                        </a>
+                    </aside>
+
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
+
     <?php include "views/footer_view.php"; ?>
 
 </body>
