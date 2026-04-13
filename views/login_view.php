41d0e31079758f1516286fc25e2f875901756a8b
alextrigueros <alextrigueros.alum@insestatut.cat>
2026-04-13 17:09:46 +0200

    estil login

--- views/login_view.php
+++ views/login_view.php
@@ -10,22 +10,40 @@
 
 <body>
     <?php include "views/header_view.php"; ?>
-    <h2>Inici de sessió</h2>
-    <!-- Si hi ha un error, el mostrem -->
-    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <div class="contenidor">
+        <div class="caixa-formulari">
+            <h2>Inici de sessió</h2>
 
-    <form action="index.php?accio=login" method="POST">
-        <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
+            <!-- Si hi ha un error, el mostrem -->
+            <?php
+            if (isset($error)) {
+                echo "<p class='missatge-error'> $error </p>";
+            }
+            ?>
 
-        <label>Contrasenya:</label><br>
-        <input type="password" name="password" required><br><br>
+            <form action="index.php?accio=login" method="POST" class="formulari-registre">
+                <div class="grup-formulari">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
 
-        <button type="submit" name="btn_login">Entrar</button>
-    </form>
-    <br>
-    <a href="index.php?accio=registre">No tinc un compte (Registra't)</a>
+                <div class="grup-formulari">
+                    <label for="password">Contrasenya:</label>
+                    <div class="contenidor-input-password">
+                        <input type="password" id="password" name="password" required>
+                        <button type="button" class="boto-revelar" onclick="togglePassword('password', this)">🔒</button>
+                    </div>
+                </div>
+
+                <button type="submit" name="btn_login" class="boto-primari-gran">Entrar</button>
+            </form>
+
+            <p class="text-peu-formulari">No tens un compte? <a href="index.php?accio=registre">Registra't</a></p>
+        </div>
+    </div>
+
     <?php include "views/footer_view.php"; ?>
+    <script src="assets/js/registre.js"></script>
 
 </body>
 
